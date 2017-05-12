e57a1881e9dae106b9a15420bb5033713023d0b2
zhaobo li <lizhaobolzb@gmail.com>
2017-05-12 19:09:18 +0800

    TRW bug

---
+++ TrappingRainWater.java
@@ -0,0 +1,36 @@
+public class Solution {
+    /**
+     * @param heights: an array of integers
+     * @return: a integer
+     */
+    public int trapRainWater(int[] heights) {
+		int left = 0;
+		int right = heights.length - 1;
+		if (left >= right) {
+			return 0;
+		}
+		int trappedWater = 0;
+		int leftHeight = heights[left];
+		int rightHeight = heights[right];
+		
+		while (left < right) {
+			while (left < right && leftHeight <= rightHeight) {
+				if (heights[left] < leftHeight) {
+					trappedWater += leftHeight - heights[left];
+				} else {
+					leftHeight = heights[left];
+				}
+				left++;
+			}
+			while (left < right && rightHeight <= leftHeight) {
+				if (heights[right] < rightHeight) {
+					trappedWater += rightHeight - heights[right];
+				} else {
+					rightHeight = heights[right];
+				}
+				right--;
+			}
+		}
+		return trappedWater;
+    }
+}
