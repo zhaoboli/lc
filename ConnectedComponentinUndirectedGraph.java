779033d1615900f70acbb871f1f06c6e8adce08a
Nate Li <Nate_Li@ad.infosys.com>
2017-05-12 14:22:17 +0800

    may-11th updated

--- ConnectedComponentinUndirectedGraph.java
+++ ConnectedComponentinUndirectedGraph.java
@@ -11,63 +11,65 @@ public class Solution {
      * @param nodes a array of Undirected graph node
      * @return a connected set of a Undirected graph
      */
+	 
     public List<List<Integer>> connectedSet(ArrayList<UndirectedGraphNode> nodes) {
         List<List<Integer>> result = new ArrayList();
-        if (nodes == null || node.size() == 0) {
+        if (nodes == null || nodes.size() == 0) {
             return result;
         }
-        int size = nodes.size(); 
-        UnionFind uf = new UnionFind(size);
-
+		
+		father = new HashMap<Integer, Integer>();
         for (UndirectedGraphNode node: nodes) {
             int curr = node.label;
+			if (!father.containsKey(curr)) {
+				father.put(curr, curr);
+			}
             for (UndirectedGraphNode neighbor: node.neighbors) {
-                uf.union(curr, neighbor.label);
+				if (!father.containsKey(neighbor.label)) {
+					father.put(neighbor.label, neighbor.label);
+				}
+                this.union(curr, neighbor.label);
             }
         }
-        return uf.connected();
+        connected(result);
+        return result;
 
     }
-
-    private class UnionFind {
-        private int[] father;
-        public UnionFind(int n) {
-            father = new int[n + 1];
-            for (int i = 1; i < n + 1; i++) {
-                father[i] = i;
-            }
-        }
-
-        public int find(int a) {
-            if(father[a] == a) {
+	
+	private HashMap<Integer, Integer> father;
+	
+	public int find(int a) {
+            if(father.get(a) == a) {
                 return a;
             }
-            return father[a] = find(father[a]);
+			father.put(a, find(father.get(a)));
+			return father.get(a);
         }
 
-        public void union(int a, int b) {
+	public void union(int a, int b) {
             int rootA = find(a);
             int rootB = find(b);
             if (rootA != rootB) {
-                father[rootA] = rootB;
+				father.put(rootA, rootB);
             }
-        }
-
-        public List<List<Integer>> connected() {
-            List<List<Integer>> result = new ArrayList();
-            HashMap<Integer, List<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
-            for ( int i = 1; i < father.size(); i++) {
-                int root = find(i); 
-                if (map.containsKey(root) {
-                    map.get(root).add(i);
-                    continue;
-                }
-                List<Integer> set = new ArrayList<Integer>();
-                set.add(i);
-                map.put(root, set)
-            }
-            return result(map.values);
-        }
     }
-    }
+	
+	private void connected(List<List<Integer>> result) {
+            HashMap<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
+			for(Integer i: father.keySet()) {
+				int root = find(i);
+				if (map.containsKey(root)) {
+					map.get(root).add(i);
+					continue;
+				}
+				List<Integer> set = new ArrayList<Integer>();
+				set.add(i);
+				map.put(root, set);
+			}
+			for(List<Integer> li: map.values()) {
+				Collections.sort(li);
+				result.add(li);
+			}
+    }    
+}
 
