79deadc8eed547d3f100fb7192b20fbf767c3d9a
zhaobo li <lizhaobolzb@gmail.com>
2017-05-19 18:14:25 +0800

    may-19-2017

---
+++ SixDegrees.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for Undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     List<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { 
+ *         label = x;
+ *         neighbors = new ArrayList<UndirectedGraphNode>(); 
+ *     }
+ * };
+ * 1------2-----4
+	\          /
+	 \        /
+	  \--3--/
+ * {1,2,3#2,1,4#3,1,4#4,2,3} and s = 1, t = 4 return 2
+ */
+public class Solution {
+    /**
+     * @param graph a list of Undirected graph node
+     * @param s, t two Undirected graph nodes
+     * @return an integer
+     */
+    public int sixDegrees(List<UndirectedGraphNode> graph,
+                          UndirectedGraphNode s,
+                          UndirectedGraphNode t) {
+        // Write your code here
+    }
+}
