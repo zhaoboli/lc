33c17d5570ebe355129bf8bad9a7adf583a3f594
zhaobo li <lizhaobolzb@gmail.com>
2017-05-22 18:42:11 +0800

    may-22-2017

---
+++ WordBreak.java
@@ -0,0 +1,9 @@
+public class Solution {
+    /**
+     * @param s: A string s
+     * @param dict: A dictionary of words dict
+     */
+    public boolean wordBreak(String s, Set<String> dict) {
+        // write your code here   
+    }
+}
