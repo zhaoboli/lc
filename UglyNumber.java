a88d6e9cda86c915317be6f84f1c212de95695d6
zhaobo <lizhaobolzb@gmail.com>
2017-05-28 11:53:12 +0800

    UglyNumber O(n)

--- UglyNumber.java
+++ UglyNumber.java
@@ -3,6 +3,7 @@ class Solution {
      * @param n an integer
      * @return the nth prime number as description
      */
+    // O(nlogn) Hash + Heap
     public int nthUglyNumber(int n) {
         PriorityQueue<Long> queue = new PriorityQueue<Long>(n);
         HashSet<Long> hash = new HashSet<Long>();
@@ -20,4 +21,28 @@ class Solution {
         }
         return (int)cur;
     }
+
+    public int nthUglyNumberOn(int n) {
+        ArrayList<Integer> queue = new ArrayList<Integer>();
+        queue.add(1);
+        int p2 = 0;
+        int p3 = 0;
+        int p5 = 0; 
+
+        for (int i = 0; i < n - 1; i++) {
+            int cur = queue.get(i);
+            while(queue.get(p2) * 2 <= cur) {
+                p2++;
+            }
+            while(queue.get(p3) * 3 <= cur) {
+                p3++;
+            }
+            while(queue.get(p5) * 5 <= cur) {
+                p5++;
+            }
+            queue.add(Math.min(queue.get(p2) * 2, Math.min(queue.get(p3) * 3, queue.get(p5) * 5)));
+        }
+        return queue.get(n - 1);
+    }
+
 };
