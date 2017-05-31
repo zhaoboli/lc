81c10737643d70edea3acef46ac2861d1b7a0fdf
zhaobo li <lizhaobolzb@gmail.com>
2017-05-31 19:43:24 +0800

    May-31-2017

---
+++ dfs/PreviousPermutation.java
@@ -0,0 +1,9 @@
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @return: A list of integers that's previous permuation
+     */
+    public ArrayList<Integer> previousPermuation(ArrayList<Integer> nums) {
+		// write your code
+    }
+}
