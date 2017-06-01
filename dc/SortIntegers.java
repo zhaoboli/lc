c37022c45ed2fd3a609895c5eb588b000c493f97
zhaobo <lizhaobolzb@gmail.com>
2017-06-01 09:49:13 +0800

    Jun1st-2017

---
+++ dc/SortIntegers.java
@@ -0,0 +1,39 @@
+public class Solution {
+    /**
+     * @param A an integer array
+     * @return void
+     */
+    //quick sort
+    public void sortIntegers(int[] A) {
+        if (A == null || A.length == 0) {
+            return;
+        }
+        quickSort(A, 0, A.length - 1);
+    }
+    private void quickSort(int[] A, int start, int end) {
+        if( start >= end) {
+            return;
+        }
+        int left = start;
+        int right = end;
+        int pivot = A[(start + end) /2];
+
+        while (left <= right) {
+            while (left <= right && A[left] < pivot) {
+                left++;
+            }
+            while (left <= right && A[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                int tmp = A[left];
+                A[left] = A[right];
+                A[right] = tmp;
+                left++;
+                right--;
+            }
+        }
+        quickSort(A, start, left);
+        quickSort(A, right, end);
+    }
+}
