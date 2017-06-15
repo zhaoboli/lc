d21e82ea434a0858907cc40ef4485ad756a6057b
zhaobo li <lizhaobolzb@gmail.com>
2017-06-15 10:26:26 +0800

    June 15th day task plan

---
+++ dp/MaximumProductSubarray.java
@@ -0,0 +1,15 @@
+/*Find the contiguous subarray within an array (containing at least one number) 
+* which has the largest product.
+
+*For example, given the array [2,3,-2,4], 
+*the contiguous subarray [2,3] has the largest product = 6.
+*/
+public class Solution {
+    /**
+     * @param nums: an array of integers
+     * @return: an integer
+     */
+    public int maxProduct(int[] nums) {
+        // write your code here
+    }
+}
