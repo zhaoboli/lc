1afd441ef76232f23181681324515324f276d374
zhaobo <lizhaobolzb@gmail.com>
2017-06-15 23:00:59 +0800

    Jun-15-2017

--- dp/game/CoinsInALineII.java
+++ dp/game/CoinsInALineII.java
@@ -17,6 +17,35 @@ public class Solution {
      * @return: a boolean which equals to true if the first player will win
      */
     public boolean firstWillWin(int[] values) {
-        // write your code here
+        if (values == null || values.length == 0) {
+           return false;
+        } 
+        int n = values.length; 
+        //when n coins left, the max value of first player can get
+        if(n <= 2) {
+            return true;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += values[i];
+        }
+
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = values[n-1];
+        dp[2] = values[n-1] + values[n-2];
+        dp[3] = values[n-3] + values[n-2];
+        //error at: values[n-i]
+        //from right to left
+        for (int i = 4; i <= n; i++)  {
+            dp[i] = Math.max(values[n-i] + Math.min(dp[i-2], dp[i-3]), 
+                    values[n-i] + values[n-i+1] + Math.min(dp[i-3], dp[i-4]));
+        }
+
+        if (dp[n] > sum / 2) {
+            return true;
+        }
+        return false;
     }
 }
