ccf47b46f246ef762a5c7c0506db4bd99f508752
zhaobo li <lizhaobolzb@gmail.com>
2017-06-19 13:26:06 +0800

    jun-19

--- dp/MaximumProductSubarray.java
+++ dp/MaximumProductSubarray.java
@@ -14,6 +14,26 @@ public class Solution {
 		if (nums == null || nums.length == 0) {
 			return 0;
 		}
+		int n = nums.length;
+		int[] min = new int[n];
+		int[] max = new int[n];
+		min[0] = max[0] = nums[0];
 		
+		int max = nums[0];
+		int min = nums[0];
+		int result = nums[0];
+		
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] > 0) {
+				max[i] = Math.max(nums[i], max[i-1] * nums[i]);
+				min[i] = Math.min(nums[i], min[i-1] * nums[i]);
+			}
+			if (nums[i] < 0) {
+				max[i] = Math.max(nums[i], min[i-1] * nums[i]);
+				min[i] = Math.min(nums[i], max[i-1] * nums[i]);
+			}
+			result = Math.max(result, max[i]);
+		}
+		return result;
     }
 }
