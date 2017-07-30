f9dbb7e05eb916e5826c151a0ee992a854f567f4
zhaobo <lizhaobolzb@gmail.com>
2017-07-30 11:28:25 +0800

    July-30 prefix sum; merge sort; Min Stack

--- prefixSum/MaximumSubarrayDifference.java
+++ prefixSum/MaximumSubarrayDifference.java
@@ -1,60 +1,60 @@
 public class Solution {
     /**
      * @param nums: A list of integers
-     * @return: An integer indicate the value of maximum difference between two Subarrays
+     * @return: An integer indicate the value of maximum difference between two
+     *          Subarrays
      */
     public int maxDiffSubArrays(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
         int len = nums.length;
-        int[] negNums = new int[len]; 
+        int[] negNums = new int[len];
         for (int i = 0; i < len; i++) {
             negNums[i] = -1 * nums[i];
         }
-        int[] leftPrefixMax = new int[len + 1];
-        int[] leftPrefixMin = new int[len + 1];
-        
-        int max = 0;
+        int[] leftPrefixMax = new int[len];
+        int[] leftPrefixMin = new int[len];
+        int[] rightPrefixMax = new int[len];
+        int[] rightPrefixMin = new int[len];
+
+        int max = Integer.MIN_VALUE;
         int min = 0;
         int sum = 0;
 
-        leftPrefixMax[0] = 0;
-        for (int i = 1; i <= len; i++) {
-            sum += nums[i-1];
-            max = Math.max(max, sum - min); 
+        for (int i = 0; i < len; i++) {
+            sum += nums[i];
+            max = Math.max(max, sum - min);
             min = Math.min(min, sum);
             leftPrefixMax[i] = max;
         }
 
-        leftPrefixMin[0] = 0;
-        sum = 0; max = 0; min = 0;
-        for (int i = 1; i <= len; i++) {
-            sum += negNums[i-1];
+        sum = 0; max = Integer.MIN_VALUE; min = 0;
+        for (int i = 0; i < len; i++) {
+            sum += negNums[i];
             max = Math.max(max, sum - min);
             min = Math.min(min, sum);
             leftPrefixMin[i] = -1 * max;
         }
 
-        int[] rightPrefixMax = new int[len + 1];
-        int[] rightPrefixMin = new int[len + 1];
-        sum = 0; max = 0; min = 0;
-        for (int i = len; i >= 1; i--) {
-            sum += nums[i-1];
+        sum = 0; max = Integer.MIN_VALUE; min = 0;
+        for (int i = len - 1; i >= 0; i--) {
+            sum += nums[i];
             max = Math.max(max, sum - min);
             min = Math.min(min, sum);
-            rightPrefixMax[i-1] = max;
+            rightPrefixMax[i] = max;
         }
 
-        sum = 0; max = 0; min = 0;
-        for (int i = len ; i >=1; i--) {
-            sum += negNums[i-1];
+        sum = 0; max = Integer.MIN_VALUE; min = 0;
+        for (int i = len - 1; i >= 0; i--) {
+            sum += negNums[i];
             max = Math.max(max, sum - min);
             min = Math.min(min, sum);
-            rightPrefixMin[i-1] = -1 * max;
+            rightPrefixMin[i] = -1 * max;
         }
+
         max = 0;
-        for (int i = 1; i < len; i++) {
+        for (int i = 0; i < len - 1; i++) {
             max = Math.max(max, leftPrefixMax[i] - rightPrefixMin[i + 1]);
             max = Math.max(max, rightPrefixMax[i + 1] - leftPrefixMin[i]);
         }
