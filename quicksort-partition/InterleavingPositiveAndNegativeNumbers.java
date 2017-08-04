277f520ad26c817d608894c04ccf6c788f4993e4
zhaobo li <lizhaobolzb@gmail.com>
2017-08-04 11:40:08 +0800

    interleaving-positive-and-negative-numbers Aug-4th

--- quicksort-partition/InterleavingPositiveAndNegativeNumbers.java
+++ quicksort-partition/InterleavingPositiveAndNegativeNumbers.java
@@ -15,7 +15,10 @@
  *　[-27,2,-22,28,-4,9,-33,2,-18,26,-17,34,-35,25,-2,2,32,35,-8]
  *　２：　但是，　即使换过来,会出现以下情况，那么也就是说这个输入起始是正数多，那么应该先排正数第一位
  * [-27,2,-22,28,-4,9,-33,2,-18,26,-17,34,-35,25,-2,2,-8,35,32]
- * 由上可见， 痛点是决定正和负，谁放第一, 然后还有其他边角问题很难考虑
+ * 由上可见， 痛点是决定正和负，谁放第一, 然后还有其他边角问题很难考虑,这样看来原地排序很难解决这个问题
+ 
+ * 既然如此，那么可以想到先把正负分开，（快排的partition）然后借助一个临时数组完成排序， 用类似归并排序的合并办法
+ × 因为正多还是负多其实就是决定第一个先放谁，那么可以加一个判断逻辑，同时设计一个函数以“先” 和 “后” 的下标为参数
  */
 class Solution {
     /**
@@ -23,6 +26,49 @@ class Solution {
      * @return: void
      */
     public void rerange(int[] A) {
-
-   }
+		if (A == null || A.length < 2) {
+			return;
+		}
+		int left = 0;
+		int right = A.length - 1;
+		while (left <= right) {
+			while (left <= right && A[left] < 0) {
+				left++;
+			}
+			while (left <= right && A[right] > 0) {
+				right--;
+			}
+			if (left <= right) {
+				int temp = A[left];
+				A[left] = A[right];
+				A[right] = temp;
+				left++;
+				right--;
+			}
+		}
+		//结束的时候left刚好过界，为第一个正数的下标
+		if (left > (A.length - 1) / 2) {
+			merge(A, 0, left, left, A.length);
+		} else {
+			merge(A, left, A.length, 0, left);
+		}
+    }
+    private void merge(int[] A, int firstStart, int firstEnd, int secondStart, int secondEnd) {
+	   int[] temp = new int[A.length];
+	   int index = 0;
+	   while (firstStart < firstEnd && secondStart < secondEnd) {
+		   temp[index++] = A[firstStart++];
+		   temp[index++] = A[secondStart++];
+	   }
+	   //此时至多还剩下一个数， 因此if即可
+	   if (firstStart < firstEnd) {
+		   temp[index] = A[firstStart++];
+	   }
+	   if (secondStart < secondEnd) {
+		   temp[index] = A[secondStart++];
+	   }
+	   for (int i = 0; i < A.length; i++) {
+		   A[i] = temp[i];
+	   }
+   }    
 }
