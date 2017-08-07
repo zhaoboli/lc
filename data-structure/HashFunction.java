0b418bd19fcf9ad96e7d05f72255a2f9e3e6f13f
zhaobo <lizhaobolzb@gmail.com>
2017-08-07 22:40:26 +0800

    Aug-6th hash function stack

--- data-structure/HashFunction.java
+++ data-structure/HashFunction.java
@@ -1,10 +1,10 @@
 class Solution {
 	/**
-	hashcode("abcd") = (ascii(a) * 333 + ascii(b) * 332 + ascii(c) *33 + ascii(d)) % HASH_SIZE 
-
-                              = (97* 333 + 98 * 332 + 99 * 33 +100) % HASH_SIZE
-
-                              = 3595978 % HASH_SIZE
+    * Prob: hash-function No: 128
+	* hashcode("abcd") = (ascii(a) * 33 3 + ascii(b) * 33 2 + ascii(c) *33 + ascii(d)) % HASH_SIZE 
+    * = (97* 33 3 + 98 * 33 2 + 99 * 33 +100) % HASH_SIZE
+    * = 3595978 % HASH_SIZE
+    *
 	*/
     /**
      * @param key: A String you should hash
@@ -16,7 +16,7 @@ class Solution {
         int len = key.length;
         long ans = 0;
         for (int i = 0; i < key.length; i++) {
-            ans = (ans * 33 + key[i]) % HASH_SIZE;
+            ans = (ans * 33 + (int)key[i]) % HASH_SIZE;
         }
         return (int)ans;
     }
