f51ef78e1bfb7198bd5fd7a5bb9982b3a5b050aa
zhaobo <lizhaobolzb@gmail.com>
2017-08-20 23:44:15 +0800

    next permutation & permutation index

--- dfs/NextPermutation.java
+++ dfs/NextPermutation.java
@@ -1,3 +1,17 @@
+/**
+ * Given a list of integers, which denote a permutation.
+ * Find the next permutation in ascending order.
+ * Prob: next-permutation No: 52
+ *
+ * For [1,3,2,3], the next permutation is [1,3,3,2]
+ * For [4,3,2,1], the next permutation is [1,2,3,4]
+ *
+ * 思路： 
+ * 先找到从右往左的第一个降序的元素 nums[index]
+ * 然后再在其右边找第一个比它大的元素，二者交换位置
+ * 然后再对 index +1 到最右的元素进行反序 
+ *
+ *
 public class Solution {
     /**
      * @param nums: an array of integers
