38d6b7c69a063d51c768c3d84c6412c6b4a33d16
zhaobo <lizhaobolzb@gmail.com>
2017-11-08 08:37:50 +0800

    Graph Valid Tree Nov-8th-2017

--- bfs/GraphValidTree.java
+++ bfs/GraphValidTree.java
@@ -1,10 +1,16 @@
-public class Solution {
-	/** 
-	* Given n = 5 and edges = [[0, 1], [0, 2], [0, 3], [1, 4]], return true.
-	* Given n = 5 and edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]], return false.	
-	*/
+/**
+ * Prob: graph-valid-tree No:178
+ * Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes),
+ * write a function to check whether these edges make up a valid tree
+ * You can assume that no duplicate edges will appear in edges.
+ * Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+ * Given n = 5 and edges = [[0, 1], [0, 2], [0, 3], [1, 4]], return true.
+ * Given n = 5 and edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]], return false.
+ */
 
-int[] father;
+//union find
+public class Solution {
+	int[] father;
 	//Union find
 	public boolean validTree(int n, int[][] edges) {
 		
@@ -43,4 +49,46 @@ int[] father;
 		}
 		father[find(a)] = father[find(b)];
 	}
+}
+//bfs
+public class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        if (edges == null) {
+            return false;
+        }
+        if (n - 1 != edges.length) {
+            return false;
+        }
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(i, new HashSet<Integer>());
+        }
+        for (int i = 0; i < edges.length; i++) {
+            int x = edges[i][0];
+            int y = edges[i][1];
+            if (!map.get(x).contains(y)) {
+                map.get(x).add(y);
+            }
+            if (!map.get(y).contains(x)) {
+                map.get(y).add(x);
+            }         
+        }
+        Set<Integer> hash = new HashSet<Integer>();
+        hash.add(0);
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.offer(0);
+
+        while (!queue.isEmpty()) {
+            Integer head = queue.poll();
+            Set<Integer> neighbors = map.get(head);
+            for (Integer i : neighbors) {
+                if (hash.contains(i)) {
+                    continue;
+                }
+                hash.add(i);
+                queue.offer(i);
+            }
+        }
+        return hash.size() == n;
+    }
 }
