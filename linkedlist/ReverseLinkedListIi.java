720d8e6fc1b302b4d407a122082f42a139786882
zhaobo <lizhaobolzb@gmail.com>
2017-11-19 22:37:19 +0800

    reverse-linked-list-ii

--- linkedlist/ReverseLinkedListIi.java
+++ linkedlist/ReverseLinkedListIi.java
@@ -59,4 +59,39 @@ public class Solution {
         beforeReverse.next = previous;
         return dummy.next;
     }
+
+    //official ans
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (m >= n || head == null) {
+            return head;
+        }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        
+        for (int i = 1; i < m; i++) {
+            if (head == null) {
+                return null;
+            }
+            head = head.next;
+        }
+        
+        ListNode premNode = head;
+        ListNode mNode = head.next;
+        ListNode nNode = mNode, postnNode = mNode.next;
+        for (int i = m; i < n; i++) {
+            if (postnNode == null) {
+                return null;
+            }
+            ListNode temp = postnNode.next;
+            postnNode.next = nNode;
+            nNode = postnNode;
+            postnNode = temp;
+        }
+        mNode.next = postnNode;
+        premNode.next = nNode;
+        
+        return dummy.next;
+    }
 }
