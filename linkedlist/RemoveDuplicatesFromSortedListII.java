74a6b4f9099e4ea3dc41d22a472c2860ef0b897e
zhaobo <lizhaobolzb@gmail.com>
2017-11-19 21:21:31 +0800

    remove duplicates from sorted list ii

--- linkedlist/RemoveDuplicatesFromSortedListII.java
+++ linkedlist/RemoveDuplicatesFromSortedListII.java
@@ -1,8 +1,13 @@
 /**
+ * Prob: remove-duplicates-from-sorted-list No: 113
+ *
  * Given a sorted linked list, delete all nodes that have duplicate numbers,
  * leaving only distinct numbers from the original list.
  * Given 1->2->3->3->4->4->5, return 1->2->5.
  * Given 1->1->1->2->3, return 2->3.
+ * 思路：
+ * 如果出现 1-> 1> -> 1，那么就是null, 因此除了比上两个数，还有记录上一次指针prev,
+ * 像本题中的情况，自然想到再加一层循环去重
 */
 
 /**
@@ -37,6 +42,29 @@ public class Solution {
                 }
             } else {
                 head = head.next;
+            }
+        }
+        return dummy.next;
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = head;
+        ListNode prev  = dummy;
+        while (curr != null && curr.next != null) {
+            if (curr.val == curr.next.val) {
+                int val = curr.val;    
+                while (curr != null && curr.val == val) {
+                    curr = curr.next;
+                }
+                prev.next = curr;
+            } else {
+                curr = curr.next;
+                prev = prev.next;
             }
         }
         return dummy.next;
