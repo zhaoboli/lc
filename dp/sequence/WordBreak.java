b70dabf5e20685ee1c6f0eff295ab107d56a2a85
zhaobo <lizhaobolzb@gmail.com>
2017-12-30 13:19:54 +0800

    word-break

--- dp/sequence/WordBreak.java
+++ dp/sequence/WordBreak.java
@@ -1,3 +1,11 @@
+Given a string s and a dictionary of words dict, determine if s can be break into a space-separated sequence of one or more dictionary words.
+Given s = "lintcode", dict = ["lint", "code"].
+Return true because "lintcode" can be break as "lint code".
+
+/**
+ * Prob: word-break No: 107
+ */
+
 public class Solution {
   import com.sun.deploy.util.StringUtils;
 
@@ -77,4 +85,42 @@ public class WordBreak {
         return max;
     }
 }
+}
+
+public class Solution {
+    /*
+     * @param s: A string
+     * @param dict: A dictionary of words dict
+     * @return: A boolean
+     */
+    public boolean wordBreak(String s, Set<String> dict) {
+        if (s == null) {
+            return false;
+        }
+        
+        boolean[] f = new boolean[s.length() + 1];
+        f[0] = true;
+        int maxLen = getMaxWordLen(dict);
+        
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = i - 1; j >= 0 && (i - j) <= maxLen; j--) {
+                if (!f[j]) {
+                    continue;
+                }
+                if (dict.contains(s.substring(j, i)) && f[j]) {
+                    f[i] = true;
+                    break;
+                }
+            }
+        }
+        return f[s.length()];
+    }
+    
+    int getMaxWordLen(Set<String> dict) {
+        int maxLen = 0;
+        for (String word: dict) {
+            maxLen = Math.max(maxLen, word.length());
+        }
+        return maxLen;
+    }
 }
