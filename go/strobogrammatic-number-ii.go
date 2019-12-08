e6c2f46668612c224b0d2deab6f0a96ccebfce75
lizhaobolzb <lizhaobolzb@gmail.com>
2019-12-08 16:23:08 +0800

    Dec-8-2019 exercise

--- go/strobogrammatic-number-ii.go
+++ go/strobogrammatic-number-ii.go
@@ -7,25 +7,52 @@ Example:
 
 Input:  n = 2
 Output: ["11","69","88","96"]
- */
+*/
 package main
+
 func isStrobogrammatic_i(num string) bool {
-nums := map[byte]byte{
-'6': '9',
-'8': '8',
-'9': '6',
-'0': '0',
-'1': '1',
+	nums := map[byte]byte{
+		'6': '9',
+		'8': '8',
+		'9': '6',
+		'0': '0',
+		'1': '1',
+	}
+	i := 0
+	j := len(num) - 1
+	for i <= j {
+		r, ok := nums[num[i]]
+		if !ok || r != num[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
 }
-i := 0
-j := len(num) -1
-for i <= j {
-r, ok := nums[num[i]]
-if !ok || r != num[j] {
-return false
+
+func findStrobogrammatic__ii(n int) []string {
+	pool := []byte{'0', '1', '6', '8', '9'}
+	res := make([]string, 0)
+	init := make([]byte, 0)
+	findStrobogrammatic(init, pool, n, &res)
+	return res
 }
-i++
-j--
-}
-return true
+
+func findStrobogrammatic(curr, pool []byte, n int, res *[]string) {
+	if len(curr) == n {
+		if isStrobogrammatic_i(string(curr)) {
+			*res = append(*res, string(curr))
+		}
+		return
+	}
+	for _, c := range pool {
+		if len(curr) == 0 && c == '0' && n > 1 {
+			continue
+		}
+		arr := make([]byte, 0)
+		arr = append(arr, curr...)
+		arr = append(arr, c)
+		findStrobogrammatic(arr, pool, n, res)
+	}
 }
--- go/strobogrammatic-number-ii_test.go
+++ go/strobogrammatic-number-ii_test.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_findStrobogrammatic__ii(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "happy: n = 1",
+			args: args{1},
+			want: []string{"0", "1", "8"},
+		}, {
+			name: "happy: n = 2",
+			args: args{2},
+			want: []string{"11", "69", "88", "96"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findStrobogrammatic__ii(tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findStrobogrammatic__ii() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
