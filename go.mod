c55ddfc9dba35d5e2f518ca8e0014abdeafa1a04
lizhaobolzb <lizhaobolzb@gmail.com>
2021-02-21 21:02:11 +0800

    2021-02-21 gitignore

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zhaoboli/lc
+
+go 1.12
---
+++ go/majority-element_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func Test_majorityElement(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := majorityElement(tt.args.nums); got != tt.want {
+				t.Errorf("majorityElement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
